4defd22bec724e35f2586c5016a9f0942364a131
Anton Romanov <rad250495@yandex.ru>
2022-07-01 16:00:28 +0700

    fix: autotest rev2

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const auth = require('./middlewares/auth');
+
+const { createUser } = require('./controllers/users');
+const { login } = require('./controllers/login');
 
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -10,8 +14,34 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
+
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.use('/users', auth, require('./routes/users'));
+app.use('/cards', auth, require('./routes/cards'));
 
 app.use((req, res, next) => {
   next(new Error('NotFoundPath'));
--- routes/users.js
+++ routes/users.js
@@ -5,40 +5,15 @@ const auth = require('../middlewares/auth');
 const {
   getUsers,
   getUser,
-  createUser,
   updateAvatar,
   getCurrentUser,
 } = require('../controllers/users');
-const { login } = require('../controllers/login');
 const { updateProfile } = require('../controllers/users');
-
-router.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string(),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser,
-);
-router.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
 
 router.use(auth);
 router.get('/me', getCurrentUser);
 router.get('/', getUsers);
+
 router.get(
   '/:userId',
   celebrate({
@@ -48,6 +23,7 @@ router.get(
   }),
   getUser,
 );
+
 router.patch(
   '/me',
   celebrate({
@@ -58,6 +34,7 @@ router.patch(
   }),
   updateProfile,
 );
+
 router.patch(
   '/me/avatar',
   celebrate({
